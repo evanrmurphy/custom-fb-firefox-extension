1e387152713332065d69989a6fc18e01608efe5b
Evan R. Murphy <evan@evanrmurphy.com>
2020-07-26 01:17:04 -0700

    Remove posts by same author
    If multiple posts by the same author appear in the news feed, only
    show the first one.

--- extension.js
+++ extension.js
@@ -1,3 +1,18 @@
+function deduplicatePostsByAuthor() {
+  const encounteredAuthorIds = {};
+  const postEls = Array.from(document.querySelectorAll('article[data-sigil*="story-div"]'));
+  for (const postEl of postEls) {
+    const postAuthorId = JSON.parse(postEl.getAttribute('data-ft')).content_owner_id_new;
+    if (encounteredAuthorIds.hasOwnProperty(postAuthorId)) {
+      postEl.innerHTML = 'hidden';
+    } else {
+      encounteredAuthorIds[postAuthorId] = true;
+    }
+  }
+}
+
+setInterval(deduplicatePostsByAuthor, 1000);
+
 // For debugging
 //
 //function testFn() { alert(getReactionsCountTextEls().map(el => el.innerText)); }
