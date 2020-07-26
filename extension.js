7b126bcaf65a7ef7a9ad6dda63eca9d41b733bd1
Evan R. Murphy <evan@evanrmurphy.com>
2020-07-26 01:24:09 -0700

    Make post removal algorithm more performant

--- extension.js
+++ extension.js
@@ -1,6 +1,14 @@
+let numPosts;
+
 function deduplicatePostsByAuthor() {
+  // For better performance, return early if the number of posts hasn't changed, meaning
+  // no new posts are detected
+  const postNodes = document.querySelectorAll('article[data-sigil*="story-div"]')
+  if (postNodes.length === numPosts) return;
+
+  numPosts = postNodes.length;
   const encounteredAuthorIds = {};
-  const postEls = Array.from(document.querySelectorAll('article[data-sigil*="story-div"]'));
+  const postEls = Array.from(postNodes);
   for (const postEl of postEls) {
     const postAuthorId = JSON.parse(postEl.getAttribute('data-ft')).content_owner_id_new;
     if (encounteredAuthorIds.hasOwnProperty(postAuthorId)) {
